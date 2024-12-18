86886891a1ee5941c3ce33670414d4520b426f72
Xamilio <Xamilio25@gmail.com>
2024-12-18 11:32:53 +0200

    Добавьте файлы проекта.

---
+++ cl33/cl33.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    const int SIZE = 12;
+    int arr[SIZE];
+    int l;
+    cout << "Enter number: ";
+    cin >> l;
+    arr[0] = l;
+    for (int i = 1; i < SIZE; i++)
+    {
+        arr[i] = arr[i - 1] * 2;
+    }
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+    
